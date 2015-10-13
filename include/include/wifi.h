e29a1503e419fc210a819236c91d0aa8f1459b08
Jorge Capona <jcapona@gmail.com>
2015-10-13 09:51:43 -0300

    Set headers

--- include/include/wifi.h
+++ include/include/wifi.h
@@ -1,9 +1,25 @@
-#include <iostream>
+#ifndef IPR_WIFI_H
+#define IPR_WIFI_H
 
-extern "C"
+#include <memory>
+
+namespace ipr
 {
- #include <stdio.h>
- #include <time.h>
- #include <iwlib.h>
-}
+    class wifi
+    {
+    public:
+        wifi();
+        ~wifi();
 
+        void connect(const std::string &network, const std::string &password);
+        void disconnect();
+        void list();
+        bool isConnected() const;
+
+    private:
+        class impl;
+        std::unique_ptr<impl> m_impl;
+    };
+}
+#endif
+// IPR_WIFI_H
--- src/wifi.cc
+++ src/wifi.cc
@@ -1,33 +1,75 @@
 #include <include/wifi.h>
+#include <string>
+#include <iostream>
 
-int main(int argc, char **argv)
+extern "C"
 {
- wireless_scan_head head;
- wireless_scan *result;
- iwrange range;
- int sock;
-
- sock = iw_sockets_open(); // Socket to kernel
-
- if (iw_get_range_info(sock, "wlan0", &range) < 0) // Get data
- {
-  printf("Error during iw_get_range_info. Aborting.\n");
-  exit(2);
- }
-
- if (iw_scan(sock, "wlan0", range.we_version_compiled, &head) < 0) // Scan
- {
-  printf("Error during iw_scan. Aborting.\n");
-  exit(2);
- }
-
- result = head.result; 
- while (NULL != result) 
- {
-  printf("%s\n", result->b.essid);
-  result = result->next;
- }
-
- exit(0);
+ #include <iwlib.h>
 }
 
+namespace ipr
+{
+    class wifi::impl
+    {
+    public:
+        impl();
+        ~impl();
+
+        void connect(const std::string &network, const std::string &password);
+        void disconnect();
+        void list();
+        bool isConnected() const;
+
+    private:
+        bool m_fd;
+        void setParameters();
+        void clear();
+    };
+}
+
+// Implementation class
+ipr::wifi::impl::impl()
+{
+
+}
+void ipr::wifi::impl::connect(const std::string &network, const std::string &password)
+{
+
+}
+
+void ipr::wifi::impl::disconnect()
+{
+
+}
+
+void ipr::wifi::impl::list()
+{
+
+}
+
+bool ipr::wifi::impl::isConnected() const
+{
+ return m_fd != -1;
+}
+
+
+// Public API
+void ipr::wifi::connect(const std::string &network, const std::string &password)
+{
+ m_impl->connect(network, password);
+}
+
+void ipr::wifi::disconnect()
+{
+ m_impl->disconnect();
+}
+
+void ipr::wifi::list()
+{
+ m_impl->list();
+}
+
+bool ipr::wifi::isConnected() const
+{
+ return m_impl->isConnected();
+}
